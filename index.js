7a95e6b7529a40298d5eba732bccd8cfe396f19c
Sabid Hasan <sabid.soft@gmail.com>
2022-11-25 19:20:59 +0600

    mongodb connection done

--- index.js
+++ index.js
@@ -2,14 +2,25 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb')
 
 // declaring variables
 const app = express()
 const port = process.env.PORT || 5000
+const client = new MongoClient(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
 
 // using middlewares
 app.use(cors())
 app.use(express.json())
+
+const run = async () => {
+    try {
+        
+    }
+    finally { }
+}
+
+run().catch(err => console.log(err))
 
 app.get('/', (req, res) => {
     res.send({ success: true, message: 'Server is running!' })
