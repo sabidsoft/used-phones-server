c23b36b030553c5ad574968e2dd5f2e9e705d96d
Sabid Hasan <sabid.soft@gmail.com>
2022-11-26 21:03:18 +0600

    booking api done

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ app.use(express.json())
 // veryfyJWT middleware
 const veryfyJWT = (req, res, next) => {
     const authHeader = req.headers.authorization
-    if(!authHeader){
+    if (!authHeader) {
         return res.status(401).send({ success: false, message: 'Unaothorized access!' })
     }
 
     const token = authHeader.split(' ')[1]
     jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
-        if(err){
+        if (err) {
             return res.status(403).send({ success: false, message: 'Forbidden access!' })
         }
         req.decoded = decoded
@@ -36,6 +36,7 @@ const run = async () => {
         const brandsCollection = client.db('usedPhones').collection('brands')
         const phonesCollection = client.db('usedPhones').collection('phones')
         const usersCollection = client.db('usedPhones').collection('users')
+        const bookingsCollection = client.db('usedPhones').collection('bookings')
 
         // generate jwt
         app.get('/jwt', async (req, res) => {
@@ -43,28 +44,28 @@ const run = async () => {
             const query = { email: email }
             const user = await usersCollection.findOne(query)
 
-            if(user){
-                const token = jwt.sign({email}, process.env.SECRET_KEY, { expiresIn: '365d' })
-                return res.send({token})
+            if (user) {
+                const token = jwt.sign({ email }, process.env.SECRET_KEY, { expiresIn: '365d' })
+                return res.send({ token })
             }
 
-            res.status(403).send({token: ''})
+            res.status(403).send({ token: '' })
         })
 
-        app.get('/brands', async(req, res) => {
+        app.get('/brands', async (req, res) => {
             const query = {}
             const brands = await brandsCollection.find(query).toArray()
             res.send(brands)
         })
 
-        app.get('/brands/:id', async(req, res) => {
+        app.get('/brands/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
             const brand = await brandsCollection.findOne(query)
             res.send(brand)
         })
 
-        app.get('/phones', async(req, res) => {
+        app.get('/phones', async (req, res) => {
             const brand = req.query.brand
             const query = { brand: brand }
             const phones = await phonesCollection.find(query).toArray()
@@ -76,11 +77,17 @@ const run = async () => {
 
             const query = { email: users.email }
             const user = await usersCollection.findOne(query)
-            if(user){
+            if (user) {
                 return res.send({ success: true, message: 'Old user!' })
             }
 
             const result = await usersCollection.insertOne(users)
+            res.send(result)
+        })
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body
+            const result = await bookingsCollection.insertOne(booking)
             res.send(result)
         })
     }
